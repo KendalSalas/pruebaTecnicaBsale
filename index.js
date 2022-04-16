c4aa53666c9458c4320dff718dfcf6f23390e52c
Kendal Salas <kensalas16.ks@gmail.com>
2022-04-16 19:42:11 -0400

    Creo uin archivo en el cual ire importando las distintas funciones para las llamadas a la api y mostrar las respuestas cuando sea necesario

---
+++ index.js
@@ -0,0 +1,8 @@
+import { listadoCategorias } from "./funcionesJS/index.js";
+
+//Archivo en el cual iré importando las distintas funciones y las iré cargando a medida que sea necesario
+
+const d = document;
+
+//Invoco la funcion listadoCategorias, para llenar el menu de las categorias con las opciones desde la BBDD
+listadoCategorias();
