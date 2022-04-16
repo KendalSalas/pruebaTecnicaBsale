4ddd7347537f1dfe6dcb8fac83f3ecebc410e155
Kendal Salas <kensalas16.ks@gmail.com>
2022-04-16 19:14:13 -0400

    Creo el archivo para hacer una query a la tabla category

---
+++ conexion/categorias.php
@@ -0,0 +1,20 @@
+<?php
+include_once 'db.php';
+
+//Clase para hacer una query a la tabla 'category'
+class Categorias extends DB
+{
+    public function obtenerCategorias()
+    {
+        $queryCategorias = "SELECT * FROM category";
+        $execCategorias  = $this->connect()->query($queryCategorias);
+
+        //Si la query se realiza correctamente, hago un return de esta
+        if ($execCategorias) {
+            return $execCategorias;
+        } else {
+            //Caso contrario, retorno un error
+            return "ERROR";
+        }
+    }
+}
