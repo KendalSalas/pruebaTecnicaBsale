a5fdf9047f0a9ad952e21ac8cbd6f071d303bca8
Kendal Salas <kensalas16.ks@gmail.com>
2022-04-17 15:22:06 -0400

    Utilizo la constante formatter en los demas listados de productos

--- funcionesJS/listadoProductos.js
+++ funcionesJS/listadoProductos.js
@@ -47,10 +47,10 @@ export const listadoProductos = async () => {
 
             if (descuento > 0) {
                 const precioOferta = formatter.format(precio - descuento);
-                precioOriginal = `<p class="card-text original">Precio $${precioFormat}</p>`;
-                txtDescuento = `<p class="card-text oferta">Oferta $${precioOferta}</p>`;
+                precioOriginal = `<p class="card-text original">Precio ${precioFormat}</p>`;
+                txtDescuento = `<p class="card-text oferta">Oferta ${precioOferta}</p>`;
             } else {
-                precioOriginal = `<p class="card-text">Precio $${precioFormat}</p>`;
+                precioOriginal = `<p class="card-text">Precio ${precioFormat}</p>`;
                 txtDescuento = '';
             }
 
@@ -115,13 +115,27 @@ export const listadoProductosCategoria = async (idCategoria, nombreCategoria) =>
         json.map(producto => {
             const { id, nombre, fotoUrl, precio, descuento } = producto; //Obtengo los datos del producto
 
+            const precioFormat = formatter.format(precio);
+
+            let precioOriginal, txtDescuento;
+
+            if (descuento > 0) {
+                const precioOferta = formatter.format(precio - descuento);
+                precioOriginal = `<p class="card-text original">Precio ${precioFormat}</p>`;
+                txtDescuento = `<p class="card-text oferta">Oferta ${precioOferta}</p>`;
+            } else {
+                precioOriginal = `<p class="card-text">Precio ${precioFormat}</p>`;
+                txtDescuento = '';
+            }
+
             //Creo un card en el cual mostraré el producto
             //Este lo guardo en la variable template
             $template += `<div class="card" style="width: 16rem;">
                             <img src="${fotoUrl}" class="card-img-top" alt="Producto ${nombre}">
                             <div class="card-body">
                                 <h5 class="card-title">${nombre}</h5>
-                                <p class="card-text">Precio $${precio}</p>
+                                ${precioOriginal}
+                                ${txtDescuento}
                             </div>
                         </div>`;
         })
@@ -179,13 +193,27 @@ export const listadoProductosNombre = async (nombre) => {
         json.map(producto => {
             const { id, nombre, fotoUrl, precio, descuento } = producto; //Obtengo los datos del producto
 
+            const precioFormat = formatter.format(precio);
+
+            let precioOriginal, txtDescuento;
+
+            if (descuento > 0) {
+                const precioOferta = formatter.format(precio - descuento);
+                precioOriginal = `<p class="card-text original">Precio ${precioFormat}</p>`;
+                txtDescuento = `<p class="card-text oferta">Oferta ${precioOferta}</p>`;
+            } else {
+                precioOriginal = `<p class="card-text">Precio ${precioFormat}</p>`;
+                txtDescuento = '';
+            }
+
             //Creo un card en el cual mostraré el producto
             //Este lo guardo en la variable template
             $template += `<div class="card" style="width: 16rem;">
                             <img src="${fotoUrl}" class="card-img-top" alt="Producto ${nombre}">
                             <div class="card-body">
                                 <h5 class="card-title">${nombre}</h5>
-                                <p class="card-text">Precio $${precio}</p>
+                                ${precioOriginal}
+                                ${txtDescuento}
                             </div>
                         </div>`;
         })
