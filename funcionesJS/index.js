614b650be57f572f127493e107a1f4a1c8c93cce
Kendal Salas <kensalas16.ks@gmail.com>
2022-04-17 15:35:45 -0400

    Ajusto el import de listadoProductosCategoria

--- funcionesJS/index.js
+++ funcionesJS/index.js
@@ -1,6 +1,6 @@
 import { listadoCategorias } from "./listadoCategorias.js";
 import { listadoProductos } from "./listadoProductos.js";
-import { listadoCategorias } from "./listadoCategorias.js";
+import { listadoProductosCategoria } from "./listadoProductosCategoria.js";
 import { listadoProductosNombre } from "./listadoProductosNombre.js";
 
 //Archivo en el cual importare las funciones y luego las exportaré al index.js de la raiz para lanzarlas cuando sea necesario
