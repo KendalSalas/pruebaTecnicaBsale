8a8978de47c965d714a3b2e0d8954c81ff64460a
Kendal Salas <kensalas16.ks@gmail.com>
2022-04-16 20:25:42 -0400

    Documento de mejor manera el archivo productos.php

--- conexion/productos.php
+++ conexion/productos.php
@@ -2,13 +2,23 @@
 
 include_once 'db.php';
 
+//Clase encargada de realizar las querys a la tabla de productos
 class Productos extends DB
 {
     public function queryProductosDestacados()
     {
+        //Query para obtener 6 productos de la tabla product ordenados aleatoriamente
+        //Estos seran los que se mostrarán la primera vez que cargue la página
         $queryProductos = "SELECT id, name, url_image, price, discount FROM product ORDER BY RAND() LIMIT 6";
         $execProductos  = $this->connect()->query($queryProductos);
 
-        return $execProductos;
+        if($execProductos){
+            //En caso de que se ejecute, hago un return de la query
+            return $execProductos;
+        } else {
+            //Caso contrario, regreso un error
+            return "ERROR";
+        }
+        
     }
 }
