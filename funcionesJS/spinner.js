b16f722ce8ba5f1de9f7947a04ada6e17afbbf6b
Kendal Salas <kensalas16.ks@gmail.com>
2022-04-16 20:02:08 -0400

    Creo archivo spinner.js, en el cual ire inyectando un spinner en el main cada vez que vaya a cargar productos

---
+++ funcionesJS/spinner.js
@@ -0,0 +1,9 @@
+//Creo una variable para almacenar un spinner 
+const $spinner = `<div class="spinner-grow text-primary" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                 </div>`;
+
+//Función para inyetar el spinner en el main mientras cargan los producto
+export const insertarSpinner = () => {
+    document.getElementById('destacados').innerHTML;
+}
