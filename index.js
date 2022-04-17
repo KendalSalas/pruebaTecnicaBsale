98b7e15739bd03c25d74fc0c4698183814f61fbf
Kendal Salas <kensalas16.ks@gmail.com>
2022-04-16 20:44:15 -0400

    Creo un event listener para cuando se haya cargado la pagina, y una vez lo hace, listo los productos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { listadoCategorias } from "./funcionesJS/index.js";
+import { listadoCategorias, listadoProductos } from "./funcionesJS/index.js";
 
 //Archivo en el cual iré importando las distintas funciones y las iré cargando a medida que sea necesario
 
@@ -6,3 +6,7 @@ const d = document;
 
 //Invoco la funcion listadoCategorias, para llenar el menu de las categorias con las opciones desde la BBDD
 listadoCategorias();
+
+d.addEventListener('DOMContentLoaded', () => {
+    listadoProductos();
+})
